8769563b30b6cbc85eff8d9c716feae2e35998d3
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-19 11:23:32 -0600

    sets direction/mvmt for snake

--- src/App.js
+++ src/App.js
@@ -14,11 +14,36 @@ class App extends Component {
   
   state = {
     food: randomFoodCoordinates(),
+    direction: 'RIGHT',
     snakeDots: [
       [0, 0],
       [2, 0],
     ]
   }
+
+componentDidMount() {
+  document.onkeydown = this.onKeyDown;
+}
+
+onKeyDown = (e) => {
+  e = e || window.event;
+  switch (e.keyCode) {
+    case 38:
+      this.setState({direction: 'UP'});
+      break;
+    case 40:
+      this.setState({direction: 'DOWN'});
+      break;
+      case 37: 
+      this.setState({directtion: "LEFT"});
+      break;
+      case 39:
+      this.setState({direction: 'RIGHT'});
+      break;
+
+  }
+}
+
   render () {
 
     return (
