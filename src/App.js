e5539b6f4fa8e9408e1da78903ec4fd4a9ac68b7
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-19 11:08:02 -0600

    sets random snake-food coordinates redo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Snake from './Snake';
 import Food from './Food';
 
+const randomFoodCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random() * (max-min+1)+min)/2) *2;
+  let y = Math.floor((Math.random() * (max-min+1)+min)/2)  *2;
+  return [x, y]
+}
+
 class App extends Component {
   
   state = {
-    food: [6, 8],
+    food: randomFoodCoordinates(),
     snakeDots: [
       [0, 0],
       [2, 0],
