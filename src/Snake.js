390a8e07f8d51c21f289314fe5ddfe386e17b5d6
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-19 13:35:03 -0600

    sets snakeCollapsed

--- src/Snake.js
+++ src/Snake.js
@@ -1,19 +1,18 @@
+
 import React from 'react';
 
 export default (props) => {
-    return (
-        <div>
-            {props.snakeDots.map((dot, i) => {
-                const style = {
-                    left: `${0}%`,
-                    top:`${2}%`,
-                }
-                return (
-                    <div className = 'snake-dot' key={1} style={style}> </div>
-                )
-            } 
-            )
-            }
-        </div>
-    )
+  return (
+    <div>
+      {props.snakeDots.map((dot, i) => {
+        const style = {
+          left: `${dot[0]}%`,
+          top: `${dot[1]}%`
+        }
+        return (
+          <div className="snake-dot" key={i} style={style}></div>
+        )
+      })}
+    </div>
+  )
 }
